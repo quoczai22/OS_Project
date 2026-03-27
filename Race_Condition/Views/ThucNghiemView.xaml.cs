7331d6ab03da9b8f542d246b90bca1dd1e171a0c
quoczai22 <kienquoctrinhfacebook@gmail.com>
2026-03-27 14:16:01 +0700

    Cập nhật code Race_Condition

--- Race_Condition/Views/ThucNghiemView.xaml.cs
+++ Race_Condition/Views/ThucNghiemView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +11,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Race_Condition.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ThucNghiemView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ThucNghiemView : UserControl
     {
-        public MainWindow()
+        public ThucNghiemView()
         {
             InitializeComponent();
         }
